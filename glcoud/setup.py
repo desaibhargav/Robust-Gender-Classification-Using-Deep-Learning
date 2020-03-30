5ee252269560fe6f289fb6f2ddfba24349736fbc
Bhargav Desai <37845875+algoromeo@users.noreply.github.com>
2020-03-31 04:12:24 +0530

    Add files via upload

---
+++ glcoud/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from setuptools import find_packages
+
+REQUIRED_PACKAGES = ['tensorflow-gpu==1.10.0', 'Keras' ]
+
+setup(name='trainer',
+      version='1.0',
+      install_requires=REQUIRED_PACKAGES,
+      packages=find_packages(),
+      description='Gender Classification',
+      author='Bhargav Desai',
+      author_email='desaibhargav98@gmail.com',
+      include_package_data=True,
+      license='MIT'
+)  
